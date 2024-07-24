f76b1b22924754ae4a21f696c428e04b9d3c4176
oliverperboni <oliverperboni@gmail.com>
2024-07-24 16:22:28 +0100

    takaing out the bg

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -4,7 +4,7 @@ function ColorMyPencil(color)
 
    vim.cmd.colorscheme(color)
 
-   vim.api.nvim_set_hl(0 ,"normal", { bg = "none" } )
-   vim.api.nvim_set_hl(0 ,"normalFloat", { bg = "none" } )
+--   vim.api.nvim_set_hl(0 ,"normal", { bg = "none" } )
+  -- vim.api.nvim_set_hl(0 ,"normalFloat", { bg = "none" } )
 end
  ColorMyPencil()
